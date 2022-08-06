35d2afa737daa860ce7c0a809e113d75bcb35d4b
Nevrik-core <57769617+Nevrik-core@users.noreply.github.com>
2022-08-06 11:33:57 +0300

    testing_errors

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 console.log(galleryItems);
+
+
+const gallery = document.querySelector('.gallery');
+
+
+const galleryMarkup = galleryItems.map(el => `
+ <a class="gallery__item" href="${el.original}">
+  <img class="gallery__image" src="${el.preview}" alt="${el.description}" />
+</a>
+`).join('');
+
+gallery.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    
+    captionsData: 'alt',
+    captions: true,
+    captionType: 'attr',
+    captionDelay: 250
+    
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+import throttle from 'lodash.throttle';
+
+const formEl = document.querySelector('.feedback-form');
+const textareaEl = document.querySelector('.feedback-form textarea');
+
+
+console.log(textareaEl);
+
+const STORAGE_KEY = 'feedback-form-state';
+
+formEl.addEventListener('submit', onFormSubmit);
+
+textareaEl.addEventListener('input', throttle(onTextareaInput, 500));
+
+populateTextarea();
+
+
+function onFormSubmit(e) {
+    e.preventDefault();
+
+    e.target.reset();
+    localStorage.removeItem(STORAGE_KEY);
+
+}
+
+
+function onTextareaInput(e) {
+    const message = e.currentTarget.value;
+    console.log(message);
+    localStorage.setItem(STORAGE_KEY, message);
+}
+
+
+
+function populateTextarea() {
+    const savedMessage = localStorage.getItem(STORAGE_KEY);
+   
+
+    if (savedMessage) {
+        console.log(savedMessage);
+       
+    }
+}
+
+
 
