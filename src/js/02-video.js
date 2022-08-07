dc512bbc58b26f718f9a52f8dc6b813dc41deef6
Nevrik-core <57769617+Nevrik-core@users.noreply.github.com>
2022-08-07 16:35:31 +0300

    task 2 and 3 complete

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,14 @@
-
-const iframe = document.querySelector('iframe');
+    import throttle from 'lodash.throttle';
+    import Player from '@vimeo/player';
+    
+    const iframe = document.querySelector('iframe');
     const player = new Vimeo.Player(iframe);
 
-    player.on('play', function() {
-        console.log('played the video!');
+    player.on('play', function(data) {
+       
+        localStorage.setItem("videoplayer-current-time", data.seconds);
     });
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
+  player.setCurrentTime(JSON.parse(localStorage.getItem("videoplayer-current-time")) || 0);
+
+player.on('timeupdate', throttle(onPlay,1000));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,59 +1,54 @@
 import throttle from 'lodash.throttle';
 
-const formEl = document.querySelector('.feedback-form');
+const form = document.querySelector('.feedback-form');
 const textareaEl = document.querySelector('.feedback-form textarea');
 const emailArea = document.querySelector('.feedback-form input');
 
-
-const formData = {};
-
 const STORAGE_KEY = 'feedback-form-state';
-
-formEl.addEventListener('submit', onFormSubmit);
-
+const formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
 
 
-formEl.addEventListener('input',throttle(saveData, 500));
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onInputChange, 500));
 
-function saveData(e) {
+
+
+
+
+// populateEmailArea();
+populateTextarea();
+
+function onInputChange(e) {
 
     formData[e.target.name] = e.target.value;
-    console.log(formData);
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+    
 }
-populateEmailArea();
-populateTextarea();
+
 
 
 
 function onFormSubmit(e) {
     e.preventDefault();
 
-    e.target.reset();
+    e.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY);
 
 }
 
 
 function populateTextarea() {
-    const savedMessage = localStorage.getItem(STORAGE_KEY);
+    const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
    
-
-    if (JSON.parse(savedMessage).message) {
-        textareaEl.value = JSON.stringify(savedMessage).message;
-       
-    }
-}
-
-function populateEmailArea() {
-    const savedMessage = localStorage.getItem(STORAGE_KEY);
-   
-
-    if (JSON.parse(savedMessage).email) {
-        emailArea.value = JSON.parse(savedMessage).email;
+    // console.log(savedMessage.message);
+    if (savedMessage) {
+        for (const el in savedMessage) {
+            if (savedMessage.hasOwnProperty(el)) {
+                textareaEl.value = savedMessage.message || '';
+                emailArea.value = savedMessage.email || '';
+            }
+        }
         
        
     }
 }
-
-
